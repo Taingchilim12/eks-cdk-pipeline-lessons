86dda6b121d1231ef5f4762a0eb6ee9a8f09174a
Jilim <chilim.taing12@gmail.com>
2026-08-25 15:05:09 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,8 +9,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 //   GITHUB_REPO -> the repository name this site will live in
 // They control the GitHub Pages URL: https://GITHUB_ORG.github.io/GITHUB_REPO/
 // ---------------------------------------------------------------------------
-const GITHUB_ORG = 'your-github-org';
-const GITHUB_REPO = 'eks-pipeline-lessons';
+const GITHUB_ORG = 'Taingchilim12';
+const GITHUB_REPO = 'eks-cdk-pipeline-lessons';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
